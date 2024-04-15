5e4e4f0dd8db23bb1bae3b45772dfad4831b95bf
Błażej Kołodziej <blazej4099@gmail.com>
2024-04-15 17:22:10 +0200

    An LCD display has been added to the project, which shows the entered symbols and informs about the correctness of the password. Most of the code has been moved to the loop() function.

--- src/code_lock.cpp
+++ src/code_lock.cpp
@@ -1,12 +1,9 @@
 #include <Arduino.h>
 #include <Keypad.h>
+#include <LiquidCrystal.h>
 
-char cipher[] = {'3', '5', '9', '3'}; //predefined cipher
-char input_num[] = {'-', '-', '-', '-'};  //entered cipher
-int i = 0;
-
-const int ROW_NUM = 4; //four rows
-const int COLUMN_NUM = 4; //four columns
+const int ROW_NUM = 4;
+const int COLUMN_NUM = 4;
 
 char keys[ROW_NUM][COLUMN_NUM] = {
   {'1','2','3', 'A'},
@@ -15,56 +12,88 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*','0','#', 'D'}
 };
 
-byte pin_rows[ROW_NUM] = {11, 10, 9, 8}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {4, 5, 6, 7};
+byte pin_column[COLUMN_NUM] = {8, 9, 10, 11};
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 
+LiquidCrystal lcd(2, 3, A3, A2, A1, A0);
+
+char password[] = {'3', '5', '9', '3'};
+char input[] = {'-', '-', '-', '-'};
+char key;
+int pos = 0;
+
 void setup(){
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
   pinMode(A4, OUTPUT);
   pinMode(A5, OUTPUT);
+
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Enter password:");
+  lcd.setCursor(0, 1);
+  lcd.print("----");
+  lcd.setCursor(0, 1);
+
   Serial.begin(9600);
 }
 
-void turnOnLEDs() {
-  if(i == 1){
-    digitalWrite(A0, LOW);
-    digitalWrite(A1, LOW);
-    digitalWrite(A2, HIGH);
-    digitalWrite(A3, LOW);
-    digitalWrite(A4, LOW);
-    digitalWrite(A5, LOW);
-  }
-  else if (i == 2){
-    digitalWrite(A3, HIGH);
-  }
-  else if (i == 3){
-    digitalWrite(A4, HIGH);
-  }
-  else if (i == 4){
-    digitalWrite(A5, HIGH);
-    if(cipher[0] == input_num[0] &&
-       cipher[1] == input_num[1] &&
-       cipher[2] == input_num[2] &&
-       cipher[3] == input_num[3]){
-         digitalWrite(A0, HIGH);
-       }
-    else{
-      digitalWrite(A1, HIGH);
-    }
-    i = 0;
-  }
-}
-
 void loop(){
-  input_num[i] = keypad.getKey();
-  if (input_num[i]){
-    Serial.println(input_num[i]);
-    i++;
+  key = keypad.getKey();
+
+  if (key == '#') {
+    if (pos != 0) {
+      pos--;
+      input[pos] = '-';
+      lcd.setCursor(pos, 1);
+      lcd.print('-');
+      lcd.setCursor(pos, 1);
+    }    
   }
-  turnOnLEDs();
+
+  else if (key == '*') {
+    if (pos == 4) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+
+      if (password[0] == input[0] &&
+          password[1] == input[1] &&
+          password[2] == input[2] &&
+          password[3] == input[3]) {
+            digitalWrite(A5, HIGH);
+            lcd.print("Correct password");
+            delay(3000);
+            digitalWrite(A5, LOW);
+      }
+
+      else {
+        digitalWrite(A4, HIGH);
+        lcd.print("Wrong password!");
+        delay(3000);
+        digitalWrite(A4, LOW);
+      }
+
+      for (int i = 0; i < 4; i++) {
+        input[i] = '-';
+      }
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Enter password:");
+      lcd.setCursor(0, 1);
+      lcd.print("----");
+      lcd.setCursor(0, 1);
+      pos = 0;
+    }    
+  }
+
+  else if (key) {
+    if (pos < 4) {
+      Serial.println(key);
+      input[pos] = key;
+      lcd.print(input[pos]);
+      pos++;
+    }
+  }
 }
